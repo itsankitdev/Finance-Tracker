1340b7fe89c157fd1a9f23280c2d18c2c278b544
Ankit <ankitk098565@gmail.com>
2026-04-05 15:52:59 +0530

    connected frontend to backend

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://finance-tracker-aebv.onrender.com/api/auth",
 });
 
 export const register = (data) => API.post("/register", data);
--- client/src/api/expenseApi.js
+++ client/src/api/expenseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/expenses",
+  baseURL: "https://finance-tracker-aebv.onrender.com/api/expenses",
 });
 
 API.interceptors.request.use((req) => {
